b5fb3bbf036dd35823f769682f6bccc873a5b12d
talon1126 <3232009404@qq.com>
2026-04-28 14:16:24 +0800

    网关服务

---
+++ services/gateway-service/src/main/java/com/talon/index12306/gatewayservice/toolkit/UserInfoDTO.java
@@ -0,0 +1,28 @@
+package com.talon.index12306.gatewayservice.toolkit;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserInfoDTO {
+
+    /**
+     * 用户 ID
+     */
+    private String userId;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+}
